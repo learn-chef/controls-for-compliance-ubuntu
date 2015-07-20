bef005e09b5eaed744bee0893ef53657538bd726
tpetchel <tpetchel@gmail.com>
2015-07-20 15:32:20 -0400

    Initial

---
+++ README.md
@@ -0,0 +1,2 @@
+# controls-for-compliance-ubuntu
+Cookbook and application code for the [Implement controls for compliance](https://learn.chef.io/controls-for-compliance/ubuntu/) tutorial on Learn Chef.
---
+++ chef-repo/cookbooks/webserver/recipes/default.rb
@@ -0,0 +1,35 @@
+#
+# Cookbook Name:: webserver
+# Recipe:: default
+#
+# Copyright (c) 2015 The Authors, All Rights Reserved.
+package 'apache2'
+
+# Enable and start the Apache2 service.
+service 'apache2' do
+  action [:enable, :start]
+end
+
+# Create the web_admin user and group.
+group 'web_admin'
+
+user 'web_admin' do
+  group 'web_admin'
+  system true
+  shell '/bin/bash'
+end
+
+# Create the pages directory under the document root directory.
+directory '/var/www/html/pages' do
+  group 'web_admin'
+  user 'web_admin'
+end
+
+# Add files to the site.
+%w(index.html pages/page1.html pages/page2.html).each do |web_file|
+  file File.join('/var/www/html', web_file) do
+    content "<html>This is #{web_file}.</html>"
+    group 'web_admin'
+    user 'web_admin'
+  end
+end
