36e597f8ab05a391777e1bbc8215980d8c80d956
tpetchel <tpetchel@gmail.com>
2015-08-07 11:55:22 -0400

    specify the file name in the audit control

--- chef-repo/cookbooks/audit/recipes/default.rb
+++ chef-repo/cookbooks/audit/recipes/default.rb
@@ -6,7 +6,7 @@
 control_group 'Validate web services' do
   control 'Ensure no web files are owned by the root user' do
     Dir.glob('/var/www/html/**/*') {|web_file|
-      it 'is not owned by the root user' do
+      it "#{web_file} is not owned by the root user" do
         expect(file(web_file)).to_not be_owned_by('root')
       end
     }
